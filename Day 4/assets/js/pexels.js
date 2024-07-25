39d6b20ef92577cf7e1f6e821c0c5bbc36994a05
Carmy93 <carmygargiulo45@gmail.com>
2024-07-25 17:47:30 +0200

    last extra

--- Day 4/assets/js/pexels.js
+++ Day 4/assets/js/pexels.js
@@ -1,6 +1,7 @@
 const load = document.getElementById('load')
 const secondaryLoad = document.getElementById('secondaryLoad')
 const btnHide = document.querySelectorAll('.btn-sm:last-child')
+const btnView = document.querySelectorAll('.btn-sm:first-child')
 const cardsTitles = document.getElementsByClassName('card-title')
 const imgs = document.getElementsByClassName('bd-placeholder-img')
 const mins = document.getElementsByTagName('small')
@@ -12,6 +13,25 @@ btnHide.forEach(button => {
         col.classList.add('d-none')
     })
 })
+
+btnView.forEach(button => {
+    button.setAttribute('data-bs-toggle', 'modal')
+    button.setAttribute('data-bs-target', '#staticBackdrop')
+    button.addEventListener('click', function(e) {
+        const buttonSelected = e.target
+        const imgDiv = buttonSelected.closest('.card')
+        const img = imgDiv.firstElementChild
+        const src = img.getAttribute('src')
+        const modalimg = document.getElementById('imgmodal')
+        modalimg.setAttribute('src', src)
+        const modalTitle = document.getElementById('staticBackdropLabel')
+        const imgTitle = buttonSelected.closest('.card-body').firstElementChild
+        modalTitle.innerText = imgTitle.innerText
+
+
+    })
+})
+
 load.addEventListener('click', function () {
     fetch("https://api.pexels.com/v1/search?query=frog", {
         headers: {
